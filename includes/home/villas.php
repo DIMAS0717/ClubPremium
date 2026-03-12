fe4cde7fc6c135fb3379df22005ebdc6043d2a9f
dimas <adimas2@ucol.mx>
2026-03-12 11:55:10 -0600

    Nuevas secciones

---
+++ includes/home/villas.php
@@ -0,0 +1,41 @@
+<section class="villas">
+    <div class="page-centered">
+
+        <div class="villas-container">
+
+            <div class="villas-texto">
+                <h2>NUESTRAS VILLAS</h2>
+                <div class="linea"></div>
+
+                <p class="intro">
+                    Villas totalmente equipadas con espacios diseñados para tu comodidad:
+                </p>
+
+                <ul class="villas-lista">
+                    <li>Jardines amplios</li>
+                    <li>Alberca semi-olímpica</li>
+                    <li>Recámaras con baño completo</li>
+                    <li>Cocina equipada</li>
+                    <li>Terraza privada</li>
+                </ul>
+
+                <a href="villas.php" class="btn-villas">
+                    EXPLORA NUESTRAS VILLAS
+                </a>
+            </div>
+
+            <div class="villas-slider">
+                <div class="slider-track">
+                    <img src="./assets/images/plantabaja.jpg" alt="">
+                    <img src="./assets/images/slider_afuera.jpg" alt="">
+                    <img src="./assets/images/slider_cocina.jpg" alt="">
+                    <img src="./assets/images/slider_cuarto.jpg" alt="">
+                    <img src="./assets/images/slider_terraza.jpg" alt="">
+                    <img src="./assets/images/slider_alberca1.jpg" alt="">
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
