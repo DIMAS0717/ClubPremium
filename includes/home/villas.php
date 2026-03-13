ad1b58a6c765db87b885e35f5c85a662b8db27e7
dimas <adimas2@ucol.mx>
2026-03-13 12:05:21 -0600

    Nueva seccion ubicacion

--- includes/home/villas.php
+++ includes/home/villas.php
@@ -1,41 +1,49 @@
 <section class="villas">
-    <div class="page-centered">
+  <div class="villas-container">
 
-        <div class="villas-container">
+    <div class="villas-texto">
+      <span class="villas-subtitle">Estancia exclusiva</span>
+      <h2>Nuestras Villas</h2>
 
-            <div class="villas-texto">
-                <h2>NUESTRAS VILLAS</h2>
-                <div class="linea"></div>
+      <p class="intro">
+        Descubre villas amplias, elegantes y totalmente equipadas, diseñadas
+        para ofrecer confort, privacidad y una experiencia inolvidable en
+        Club Santiago. Cada espacio ha sido pensado para combinar descanso,
+        funcionalidad y vistas privilegiadas hacia jardines y áreas comunes.
+      </p>
 
-                <p class="intro">
-                    Villas totalmente equipadas con espacios diseñados para tu comodidad:
-                </p>
+      <ul class="villas-lista">
+        <li>Jardines amplios y entorno natural</li>
+        <li>Alberca semi-olímpica</li>
+        <li>Recámaras con baño completo</li>
+        <li>Cocina equipada y espacios funcionales</li>
+        <li>Terraza privada con vista al jardín</li>
+      </ul>
 
-                <ul class="villas-lista">
-                    <li>Jardines amplios</li>
-                    <li>Alberca semi-olímpica</li>
-                    <li>Recámaras con baño completo</li>
-                    <li>Cocina equipada</li>
-                    <li>Terraza privada</li>
-                </ul>
-
-                <a href="villas.php" class="btn-villas">
-                    EXPLORA NUESTRAS VILLAS
-                </a>
-            </div>
-
-            <div class="villas-slider">
-                <div class="slider-track">
-                    <img src="./assets/images/plantabaja.jpg" alt="">
-                    <img src="./assets/images/slider_afuera.jpg" alt="">
-                    <img src="./assets/images/slider_cocina.jpg" alt="">
-                    <img src="./assets/images/slider_cuarto.jpg" alt="">
-                    <img src="./assets/images/slider_terraza.jpg" alt="">
-                    <img src="./assets/images/slider_alberca1.jpg" alt="">
-                </div>
-            </div>
-
-        </div>
-
+      <a href="villas.php" class="btn-villas">Explora nuestras villas</a>
     </div>
+
+    <div class="villas-galeria">
+      <div class="villa-img villa-main">
+        <img src="./assets/img/fondos/plantabaja.jpg" alt="Vista principal de villa" loading="lazy">
+      </div>
+
+      <div class="villa-img">
+        <img src="./assets/img/fondos/slider_afuera.jpg" alt="Exterior de villa" loading="lazy">
+      </div>
+
+      <div class="villa-img">
+        <img src="./assets/img/fondos/slider_cocina.jpg" alt="Cocina equipada" loading="lazy">
+      </div>
+
+      <div class="villa-img">
+        <img src="./assets/img/fondos/slider_cuarto.jpg" alt="Recámara de villa" loading="lazy">
+      </div>
+
+      <div class="villa-img">
+        <img src="./assets/img/fondos/slider_terraza.jpg" alt="Terraza privada" loading="lazy">
+      </div>
+    </div>
+
+  </div>
 </section>
