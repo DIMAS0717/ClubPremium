9ce6f97f8c9393fd9fe9b9d8dd77ffc237474872
dimas <adimas2@ucol.mx>
2026-03-28 09:49:12 -0600

    DARK MODE

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ try {
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
-<body>
+<body class="dark-mode">
 
 <?php include __DIR__ . '/includes/header.php'; ?>
 
