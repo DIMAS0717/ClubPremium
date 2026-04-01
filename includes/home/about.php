c3c2f4307c9a99aec78dfa1a2a7b46ad4f8acfd1
dimas <adimas2@ucol.mx>
2026-04-01 02:20:02 -0600

    acomodar responsive a su .css

--- includes/home/about.php
+++ includes/home/about.php
@@ -7,7 +7,7 @@
       <div class="divider_about"></div>
 
       <p>
-        Club Santiago Real Estate es la agencia líder en propiedades de lujo.
+        Club Santiago es una inmobiliaria líder en propiedades vacacionales,
         Nos especializamos en conectar a personas extraordinarias con hogares
         excepcionales. Nuestro compromiso es brindar un servicio personalizado,
         transparente y de clase mundial.
@@ -16,7 +16,7 @@
       <ul class="about-list">
         <li>Asesoría legal y financiera completa</li>
         <li>Gestión de propiedades y mantenimiento</li>
-        <li>Concierge de lujo 24/7</li>
+        <li>Propiedades vacacionales <br>en Manzanillo</li>
       </ul>
 
       <a href="contacto.php" class="about-link">Contacta a nuestro equipo</a>
